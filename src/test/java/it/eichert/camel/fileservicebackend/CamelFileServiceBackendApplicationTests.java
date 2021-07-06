fe747be2c2b3fbf40db3cb00c86e752bce55fcfd
ceichert <ceichert84@gmail.com>
2021-07-06 14:20:25 +0200

    initial

---
+++ src/test/java/it/eichert/camel/fileservicebackend/CamelFileServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.eichert.camel.fileservicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelFileServiceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
